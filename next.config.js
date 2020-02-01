82c604be2f44391468b8f4dc619674b3a8da8793
Koshin Mariano <koshin.mariano@gmail.com>
2020-02-01 15:12:46 -0700

    Adding Tailwind CSS

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS({
   env: {
     avayaAccountSID: process.env.AVAYA_ACCOUNT_SID,
     avayaAccountToken: process.env.AVAYA_ACCOUNT_TOKEN,
     googleAPIKey: process.env.GOOGLE_API_KEY,
     googleSearchCX: process.env.GOOGLE_SEARCH_CX
   }
-};
+});
