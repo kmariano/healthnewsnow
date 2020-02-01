9fccac00e9c6df14f80509ada68db19f3940f8e1
Andrew <me@iamandrew.io>
2020-02-01 15:05:02 -0700

    put links on homepage

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,15 @@ const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <Link href='signup'>
+        <h3>Sign Up</h3>
+      </Link>
+      <Link href='signup/topics'>
+        <h3>Sign Up Topics</h3>
+      </Link>
+      <Link href='signup/success'>
+        <h3>Sign Up Success</h3>
+      </Link>
       <Link href='news-search'>
         <h3>News Search</h3>
       </Link>
