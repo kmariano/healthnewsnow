7d08db1827b5084f000e8d425479148d8fff3b03
Andrew <me@iamandrew.io>
2020-02-02 13:30:25 -0700

    styling success

--- pages/signup/success/index.js
+++ pages/signup/success/index.js
@@ -1,6 +1,17 @@
 import TopBar from '../../../components/top-bar';
 import './index.css';
 
+const DATA = [
+  {
+    title: 'Coronavirus',
+    articles: ['Something bad is happening!', 'Some person just got infected...']
+  },
+  {
+    title: 'Chemical Spill',
+    articles: ['Something else bad is happening!', 'Some other person just got infected...']
+  }
+]
+
 const SignupSuccess = () => {
   return (
     <>
@@ -13,6 +24,17 @@ const SignupSuccess = () => {
       <div className='signup-success__max-width'>
         <p className='signup-success__title--big'>User's Dashboard</p>
         <p className='signup-success__title'>Topics Selected</p>
+
+        {DATA.map((item, i) => {
+          return (
+            <div key={i} className={i % 2 === 0 ? 'signup-success__container--green' : 'signup-success__container'}>
+              <p className='signup-success__article-title'>{item.title}</p>
+              {item.articles.map(a => {
+                return <p className='signup-success__article-item'>{a}</p>
+              })}
+            </div>
+          );
+        })}
       </div>
     </>
   );
