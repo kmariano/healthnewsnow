e8d17902cc27b387ea3081b7f26ca88d0f819f44
Andrew <me@iamandrew.io>
2020-02-01 22:06:51 -0700

    render cards on topics page

--- pages/signup/topics/index.js
+++ pages/signup/topics/index.js
@@ -1,5 +1,15 @@
+import Card from '../../../components/card';
 import TopBar from '../../../components/top-bar';
 import './index.css';
+
+const data = [
+  {title: 'Something Bad', image: 'https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/322/322223/herpesvirus-in-purple.jpg?w=1155&h=1541'},
+  {title: 'Something Bad', image: 'https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/322/322223/herpesvirus-in-purple.jpg?w=1155&h=1541'},
+  {title: 'Something Bad', image: 'https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/322/322223/herpesvirus-in-purple.jpg?w=1155&h=1541'},
+  {title: 'Something Bad', image: 'https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/322/322223/herpesvirus-in-purple.jpg?w=1155&h=1541'},
+  {title: 'Something Bad', image: 'https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/322/322223/herpesvirus-in-purple.jpg?w=1155&h=1541'},
+  {title: 'Something Bad', image: 'https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/322/322223/herpesvirus-in-purple.jpg?w=1155&h=1541'}
+]
 
 const SignupTopics = () => {
   return (
@@ -12,8 +22,18 @@ const SignupTopics = () => {
 
       <div className='topics__container'>
         <p className='topics__title'>Known Illnesses</p>
+        <div className='topics__card-wrap'>
+          {data.map((item, i) => {
+            return <Card key={i} title={item.title} image={item.image} />;
+          })}
+        </div>
 
         <p className='topics__title'>Potential Threats</p>
+        <div className='topics__card-wrap'>
+          {data.map((item, i) => {
+            return <Card key={i} title={item.title} image={item.image} />;
+          })}
+        </div>
       </div>
     </>
   );
