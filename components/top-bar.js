4e1fd88fcad2a0e2237cac3b4e631eced481aa0e
Andrew <me@iamandrew.io>
2020-02-01 17:13:10 -0700

    fix top-bar image

--- components/top-bar.js
+++ components/top-bar.js
@@ -4,7 +4,7 @@ const TopBar = () => {
   return (
     <div className='top-bar__container'>
       <div className='top-bar__container_wrap'>
-        <image style={{ height: 75, width: 200, background: 'red' }} src="/logo-white.png" />
+        <img className='top-bar__image' src="/logo-white.png" />
 
         <Link href='/signup'>
           <button style={{ color: 'white' }}>subscribe</button>
