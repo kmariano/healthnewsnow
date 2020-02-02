61eb82a24c80d313b1d66937da308c54d26cb436
Andrew <me@iamandrew.io>
2020-02-01 20:45:58 -0700

    add alt color prop to topBar for inverse colors

--- components/top-bar/index.js
+++ components/top-bar/index.js
@@ -2,21 +2,30 @@ import Link from 'next/link';
 
 import './index.css';
 
-const TopBar = () => {
+const TopBar = ({ altColor = false }) => {
   return (
-    <div className='top-bar__container'>
+    <div className={`top-bar__container ${altColor ? 'top-bar__container--alt' : ''}`}>
       <div className='top-bar__container_wrap'>
-        <img className='top-bar__image' src="/logo-white.png" />
+        <img
+          className='top-bar__image'
+          src={altColor ? "/logo-blue.png" : "/logo-white.png"}
+        />
 
         <div className='top-bar__button-container'>
           <Link href='/signup'>
-            <button className='top-bar__button'>subscribe</button>
+            <button className={`top-bar__button ${altColor ? 'top-bar__button--alt' : ''}`}>
+              subscribe
+            </button>
           </Link>
           <Link href='/about'>
-            <button className='top-bar__button'>about</button>
+            <button className={`top-bar__button ${altColor ? 'top-bar__button--alt' : ''}`}>
+              about
+            </button>
           </Link>
           <Link href='/faq'>
-            <button className='top-bar__button'>faq</button>
+            <button className={`top-bar__button ${altColor ? 'top-bar__button--alt' : ''}`}>
+              faq
+            </button>
           </Link>
         </div>
       </div>
