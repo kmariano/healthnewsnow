014334657d84fa6a4ffb35b1da33d8ac81ab8057
Olu Ayandosu <oayandosu@gmail.com>
2020-02-01 23:18:28 -0700

    Update users service

--- services/users.js
+++ services/users.js
@@ -1,22 +1,31 @@
 const { Firestore } = require("@google-cloud/firestore");
 const firestore = new Firestore();
 
-export const createUser = ({ name, phoneNumber, city, state }) => {
+export const create = async ({ name, phoneNumber, city, state }) => {
   //Check to see if the phone number already exists
   //return an error if it does
   const userCollection = firestore.collection("users");
   const userQuery = firestore
     .collection("users")
     .where("phoneNumber", "==", phoneNumber);
-  userQuery.get().then(existingUser => {
-    if (existingUser.exists) {
-      return existingUser;
-    } else {
-      userCollection
-        .add({ name, phoneNumber, city, state, topics: [] })
-        .then(newUser => {
-          return newUser;
-        });
-    }
+
+  const userQuerySnapshot = await userQuery.limit(1).get();
+
+  if (!userQuerySnapshot.empty) {
+    const user = userQuerySnapshot.docs[0];
+    return {
+      id: user.id,
+      ...user.data()
+    };
+  }
+
+  const newUserDocReference = await userCollection.add({
+    name,
+    phoneNumber,
+    city,
+    state,
+    topics: []
   });
+  const newUser = await newUserDocReference.get();
+  return { id: newUser.id, ...newUser.data() };
 };
