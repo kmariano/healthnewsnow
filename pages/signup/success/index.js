9047c48581aca07358cc4db408db20b88e880283
Andrew <me@iamandrew.io>
2020-02-02 13:21:09 -0700

    work on success page

--- pages/signup/success/index.js
+++ pages/signup/success/index.js
@@ -1,5 +1,21 @@
+import TopBar from '../../../components/top-bar';
+import './index.css';
+
 const SignupSuccess = () => {
-  return <div>Success!</div>;
+  return (
+    <>
+      <TopBar altColor />
+
+      <div className='signup-success__banner'>
+        DASHBOARD
+      </div>
+
+      <div className='signup-success__max-width'>
+        <p className='signup-success__title--big'>User's Dashboard</p>
+        <p className='signup-success__title'>Topics Selected</p>
+      </div>
+    </>
+  );
 }
 
 export default SignupSuccess;
