814542b906438bf029110ba6ea27f854190dd664
Andrew <me@iamandrew.io>
2020-02-02 12:21:52 -0700

    fix card title

--- components/topics-list/index.js
+++ components/topics-list/index.js
@@ -1,14 +1,18 @@
 import _ from "lodash";
-import "./styles.css";
+
 import Card from "../card";
+import "./styles.css";
+
 const TopicsList = ({ topics, onTopicsUpdated }) => {
   if (_.isEmpty(topics)) return null;
+
   return (
     <div className="topic-list">
       {topics.map(topic => {
         return (
           <Card
-            topic={topic.title}
+            key={topic.title}
+            title={topic.title}
             imageUrl={topic.imageUrl}
             selected={topic.selected}
             onClick={onTopicsUpdated}
